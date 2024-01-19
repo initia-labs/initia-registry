e0b3feede41cb460dd1855c18b5886200cd05a0b
beer-1 <147697694+beer-1@users.noreply.github.com>
2024-01-19 14:28:41 +0900

    test

--- .github/workflows/utility/validate_data.py
+++ .github/workflows/utility/validate_data.py
@@ -64,6 +64,7 @@ def readSLIP44():
           lines.append(line)
     if lines:
       for line in lines:
+        print(line)
         coinNumber = int(line[0:line.find(" ")])
         if(line.find("[") > 0):
           pretty = line[line.find("[")+1:line.find("]")]
