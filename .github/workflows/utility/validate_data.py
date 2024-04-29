2af5352dca6c92059d31d4fef70fc6bf034c8be1
ALPAC-4 <contact.alpac4@gmail.com>
2024-04-29 18:42:36 +0900

    feat: remove token validation and add chain name validation

--- .github/workflows/utility/validate_data.py
+++ .github/workflows/utility/validate_data.py
@@ -17,64 +17,10 @@ def checkChains():
           chainSchema = json.load(open(os.path.join(rootdir, chainjson)))
           assetlistjson = os.path.join(networkfolder, chainfolder, "assetlist.json")
           print(assetlistjson + "  - " + str(os.path.exists(assetlistjson)))
-          if not os.path.exists(assetlistjson):
-              continue
-          assetlistSchema = json.load(open(os.path.join(rootdir, assetlistjson)))
-          bases = []
-          if "assets" in assetlistSchema:
-            if assetlistSchema["assets"]:
-              for asset in assetlistSchema["assets"]:
-                assetDenoms = []
-                if "denom_units" in asset:
-                  if asset["denom_units"]:
-                    for unit in asset["denom_units"]:
-                      if "denom" in unit:
-                        assetDenoms.append(unit["denom"])
-                      if "aliases" in unit:
-                        for alias in unit["aliases"]:
-                          assetDenoms.append(alias)
-                  else:
-                    raise Exception("'denon_units' array doesn't contain any units")
-                if "base" in asset:
-                  if asset["base"] in assetDenoms:
-                    bases.append(asset["base"])
-                if "images" in asset:
-                  for image in asset["images"]:
-                    if "png" in image:
-                      validateRawGithubContent(image["png"])
-                    if "svg" in image:
-                      validateRawGithubContent(image["svg"])
-                if "logo_URIs" in asset:
-                  if "png" in asset["logo_URIs"]:
-                    validateRawGithubContent(asset["logo_URIs"]["png"])
-                  if "svg" in asset["logo_URIs"]:
-                    validateRawGithubContent(asset["logo_URIs"]["svg"])
-            else:
-              raise Exception("'assets' array doesn't contain any tokens")
-          else:
-            raise Exception("assetlist schema doesn't contain 'assets' array")
-          if "fees" in chainSchema:
-            if "fee_tokens" in chainSchema["fees"]:
-              if chainSchema["fees"]["fee_tokens"]:
-                for token in chainSchema["fees"]["fee_tokens"]:
-                  if "denom" in token:
-                    if token["denom"] not in bases:
-                      raise Exception(token["denom"] + " is not in bases")
-              else:
-                raise Exception("'fee_tokens' array doesn't contain any tokens")
-          if "staking" in chainSchema:
-            if "staking_tokens" in chainSchema["staking"]:
-              if chainSchema["staking"]["staking_tokens"]:
-                for token in chainSchema["staking"]["staking_tokens"]:
-                  if "denom" in token:
-                    if token["denom"] not in bases:
-                      raise Exception(token["denom"] + " is not in bases")
-                  else:
-                    raise Exception("token doesn't contain 'denom' string")
-            else:
-              raise Exception("'fees' object doesn't contain 'staking_tokens' array")
-          else:
-            print("[OPTIONAL - Keplr Compliance] chain schema doesn't contain 'staking' object")
+          
+          if chainfolder != chainSchema["chain_name"]:
+            raise Exception("folder name must be same with chain name (" + chainfolder +")")
+
           # raw.githubusercontent uri check
           if "images" in chainSchema:
             for image in chainSchema["images"]:
