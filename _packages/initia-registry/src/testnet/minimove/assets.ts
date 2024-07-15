3f7769ed249e4fa5c49f2522d2c30b7a9a88e3ad
ALPAC-4 <81249838+ALPAC-4@users.noreply.github.com>
2024-07-15 19:05:45 +0900

    feat: add executor api (#157)

--- _packages/initia-registry/src/testnet/minimove/assets.ts
+++ _packages/initia-registry/src/testnet/minimove/assets.ts
@@ -148,6 +148,35 @@ const info: AssetList = {
         svg: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/tucana/images/TUCANA.svg",
       },
     },
+    {
+      description: "The INIT Liquid Staking Token",
+      denom_units: [
+        {
+          denom:
+            "ibc/7F4EE5B281CD4EEF975BD4A892A275D9AB4987BDB70974763A438524631347E5",
+          exponent: 0,
+        },
+        {
+          denom: "milkINIT",
+          exponent: 6,
+        },
+      ],
+      base: "ibc/7F4EE5B281CD4EEF975BD4A892A275D9AB4987BDB70974763A438524631347E5",
+      display: "milkINIT",
+      name: "INIT Liquid Staking Token",
+      symbol: "milkINIT",
+      coingecko_id: "",
+      images: [
+        {
+          png: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/milkyway/images/milkINIT.png",
+          svg: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/milkyway/images/milkINIT.svg",
+        },
+      ],
+      logo_URIs: {
+        png: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/milkyway/images/milkINIT.png",
+        svg: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/milkyway/images/milkINIT.svg",
+      },
+    },
   ],
 };
 export default info;
--- _packages/initia-registry/src/testnet/miniwasm/chain.ts
+++ _packages/initia-registry/src/testnet/miniwasm/chain.ts
@@ -79,6 +79,7 @@ const info: Chain = {
   metadata: {
     op_bridge_id: "2",
     op_denoms: ["uinit"],
+    executor_uri: "https://executor.miniwasm-1.initia.xyz",
     ibc_channels: [
       {
         chain_id: "initiation-1",
