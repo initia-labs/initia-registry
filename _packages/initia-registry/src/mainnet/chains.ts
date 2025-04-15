74c87b38f14d8dbf1546eeecb4a0aca5843db9a7
ALPAC-4 <81249838+ALPAC-4@users.noreply.github.com>
2025-04-15 04:32:30 +0000

    [BOT] Update initia-registry to 0.2.7

--- _packages/initia-registry/src/mainnet/chains.ts
+++ _packages/initia-registry/src/mainnet/chains.ts
@@ -1,8 +1,9 @@
 import { Chain } from "@initia/initia-registry-types";
 
+import * as _yominet from "./yominet";
 import * as _initia from "./initia";
 import * as _echelon from "./echelon";
 
-const chains: Chain[] = [_initia.chain, _echelon.chain];
+const chains: Chain[] = [_yominet.chain, _initia.chain, _echelon.chain];
 
 export default chains;
