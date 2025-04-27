a14594f8ae779922484ad60ffb96f898c433d488
ALPAC-4 <81249838+ALPAC-4@users.noreply.github.com>
2025-04-27 17:06:18 +0000

    [BOT] Update initia-registry to 1.0.4

--- _packages/initia-registry/src/testnet/assets.ts
+++ _packages/initia-registry/src/testnet/assets.ts
@@ -8,6 +8,7 @@ import * as _minievm from "./minievm";
 import * as _milkyway from "./milkyway";
 import * as _infinityg from "./infinityg";
 import * as _inertia_legacy from "./inertia_legacy";
+import * as _embr_old from "./embr_old";
 import * as _embr from "./embr";
 import * as _culinaris from "./culinaris";
 
@@ -20,6 +21,7 @@ const assets: AssetList[] = [
   _milkyway.assets,
   _infinityg.assets,
   _inertia_legacy.assets,
+  _embr_old.assets,
   _embr.assets,
   _culinaris.assets,
 ];
