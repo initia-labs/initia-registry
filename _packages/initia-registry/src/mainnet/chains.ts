7b63c819b30a0081bde07b6a65dbd35b9263434d
ALPAC-4 <81249838+ALPAC-4@users.noreply.github.com>
2025-04-28 04:31:08 +0000

    [BOT] Update initia-registry to 1.0.4

--- _packages/initia-registry/src/mainnet/chains.ts
+++ _packages/initia-registry/src/mainnet/chains.ts
@@ -2,6 +2,7 @@ import { Chain } from "@initia/initia-registry-types";
 
 import * as _zaarmainnet from "./zaarmainnet";
 import * as _yominet from "./yominet";
+import * as _renanuwa from "./renanuwa";
 import * as _rave from "./rave";
 import * as _moo from "./moo";
 import * as _intergaze from "./intergaze";
@@ -14,6 +15,7 @@ import * as _civitia from "./civitia";
 const chains: Chain[] = [
   _zaarmainnet.chain,
   _yominet.chain,
+  _renanuwa.chain,
   _rave.chain,
   _moo.chain,
   _intergaze.chain,
